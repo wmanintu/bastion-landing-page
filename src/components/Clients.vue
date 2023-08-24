7fdf232c3b8a7d6eb0810323404b4ebf8648fb50
joshcesana <joshuacesana@gmail.com>
2023-08-24 11:32:59 -0400

    Add Clients component

---
+++ src/components/Clients.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="dark-bg is-relative">
+        <img class="dots-wide" src="../assets/dots-wide2.svg" alt="Decorative dots image"/>
+        <div class="my-container">
+            <h2 class="has-text-white has-text-centered is-size-2 has-text-weight-bold is-uppercase">
+                What Clients Say About Us
+            </h2>
+            <!-- TODO: get slider -->
+        </div>
+        <img class="dots-narrow" src="../assets/dots-narrow2.svg" alt="Decorative dots image"/>
+    </section>
+</template>
+
+<style scoped>
+.my-container {
+    padding: 11.75rem 0 7.5rem;
+}
+
+.dots-wide, .dots-narrow {
+    position: absolute;
+}
+
+.dots-wide {
+    top: -6.6rem;
+    right: 0;
+}
+
+.dots-narrow {
+    bottom: -5rem;
+    left: 2rem ;
+}
+
+h2 {
+    letter-spacing: 0.8px;
+}
+</style>
